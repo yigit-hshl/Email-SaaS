ddd13ff67b67e2096e7aa907a3bea0b51104ee8e
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2025-09-16 15:24:17 +0200

    Added basic clerk-authentication

--- email-saas/src/app/page.tsx
+++ email-saas/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
-  return <h1 className="text-red-600">Hello World!</h1>
+  return <Button>Hello World</Button>
 }

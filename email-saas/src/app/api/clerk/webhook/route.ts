883d1453a35d334416398bcc141263c91861dcbf
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2025-09-21 21:13:34 +0200

    Added db.user.create() to record new user credentials

--- email-saas/src/app/api/clerk/webhook/route.ts
+++ email-saas/src/app/api/clerk/webhook/route.ts
@@ -1,9 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // /api/clerk/webhook
 
-export const POST = async(req: Request) => {
-  const {data} = await req.json()
-  console.log('clerk webhook received', data)
+import { db } from "@/server/db"
 
-  return new Response('Webhook received', { status:200 })
+export const POST = async (req: Request) => {
+  const { data } = await req.json()
+  console.log('clerk webhook received', data)
+  //const emailAddress = data.email_addresses[0].email_address
+  const emailAddress = "test@test.com"
+  const firstName = data.first_name
+  const lastName = data.last_name
+  const imageUrl = data.image_url
+  const id = data.id
+
+  await db.user.create({
+    data: {
+      id: id,
+      emailAddress: emailAddress,
+      firstName: firstName,
+      lastName: lastName,
+      imageUrl: imageUrl
+    },
+  })
+
+  console.log('User created.')
+
+  return new Response('Webhook received', { status: 200 })
 }

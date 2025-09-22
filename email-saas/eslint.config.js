6db0efc4ee6b67450daa497f08d82a5b16b579db
yigit-hshl <56076926+yigit-hshl@users.noreply.github.com>
2025-09-22 19:30:53 +0200

    Modified eslint rules

--- email-saas/eslint.config.js
+++ email-saas/eslint.config.js
@@ -20,6 +20,11 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
         { prefer: "type-imports", fixStyle: "inline-type-imports" },
